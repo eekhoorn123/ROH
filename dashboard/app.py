7aa3e6be2abd0a5cfb5cc523bc92c7cef4ce13a0
eekhoorn123 <39386964+eekhoorn123@users.noreply.github.com>
2018-08-29 22:27:58 +0200

    update

--- dashboard/app.py
+++ dashboard/app.py
@@ -25,7 +25,10 @@ server = app.server
 
 widthScreen = (GetSystemMetrics(0)-20)
 heightScreen = (GetSystemMetrics(1)-200)
-deel_heightScreen = (heightScreen/3)
+threeeight_heightScreen = (heightScreen/8*3)
+twoeight_heightScreen = (heightScreen/8*2)
+fifteen_heightScreen = (heightScreen/15)
+thirty_heightScreen = (heightScreen/30)
 
 dict_links_maps = {'sites': 'maps/all_sites.html',
               'vLearnC':'maps/all_LearnC.html',
@@ -35,6 +38,21 @@ dict_links_maps = {'sites': 'maps/all_sites.html',
               'vNC':'maps/all_NC.html',
               'vHF':'maps/all_HF.html'}
 
+dict_links_numbers = {'sites': '0',
+              'vLearnC':'1',
+              'vWFS':'2',
+              'vCFS':'3',
+              'vWASH':'4',
+              'vNC':'5',
+              'vHF':'6'}
+
+dict_links_text = {'sites': 'People in Refugee Camps',
+              'vLearnC':'Learning Centers',
+              'vWFS':'Women Friendly Spaces',
+              'vCFS':'Child Friendly Spaces',
+              'vWASH':'WASH Facilities',
+              'vNC':'Nutrition Centers',
+              'vHF':'Health Facilities'}
 
 dict_links_boxplot = {'sites': 'https://plot.ly/~Eekhoorn234/15.embed',
               'vLearnC':'https://plot.ly/~Eekhoorn234/0.embed',
@@ -43,6 +61,14 @@ dict_links_boxplot = {'sites': 'https://plot.ly/~Eekhoorn234/15.embed',
               'vWASH':'https://plot.ly/~Eekhoorn234/6.embed',
               'vNC':'https://plot.ly/~Eekhoorn234/8.embed',
               'vHF':'https://plot.ly/~Eekhoorn234/10.embed'}
+
+dict_links_colours = {'sites': 'black',
+              'vLearnC':'orange',
+              'vWFS':'pink',
+              'vCFS':'lightblue',
+              'vWASH':'blue',
+              'vNC':'lightorange',
+              'vHF':'red'}
 
 colors = {
     'background': '#FFFFFF',
@@ -63,24 +89,44 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},
                                                                                                    'marginTop':0}))])]),
                                                          #html.A(html.Img(src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe-inverted.png"))
                                         html.Div(className="body", children=[html.Div(dcc.RadioItems(id='select_maps',
-                                                                                                    options=[{'label': 'Campsites ', 'value': 'sites'},
-                                                                                                             {'label': 'Learning Centers ', 'value': 'vLearnC'},
-                                                                                                             {'label': 'Women Friendly Spaces ', 'value': 'vWFS'},
-                                                                                                             {'label': 'Child Friendly Spaces ', 'value': 'vCFS'},
-                                                                                                             {'label': 'WASH Infrastructure ', 'value': 'vWASH'},
-                                                                                                             {'label': 'Nutrition Centers ', 'value': 'vNC'},
-                                                                                                             {'label': 'Health Facilities ', 'value': 'vHF'}],
+                                                                                                    options=[{'label': 'Campsites___', 'value': 'sites'},
+                                                                                                             {'label': 'Learning Centers___', 'value': 'vLearnC'},
+                                                                                                             {'label': 'Women Friendly Spaces___ ', 'value': 'vWFS'},
+                                                                                                             {'label': 'Child Friendly Spaces___ ', 'value': 'vCFS'},
+                                                                                                             {'label': 'WASH Infrastructure___ ', 'value': 'vWASH'},
+                                                                                                             {'label': 'Nutrition Centers___ ', 'value': 'vNC'},
+                                                                                                             {'label': 'Health Facilities___', 'value': 'vHF'}],
                                                                                                     value=['sites'],
                                                                                                     labelStyle={'display': 'inline-block'},
                                                                                                     style={'textAlign': 'center',
-                                                                                                           'color': 'orange',
+                                                                                                           'color': 'black',
                                                                                                            'fontFamily': 'verdana',
                                                                                                            'fontSize': 12}),className="row"),
                                                                             html.Div(children=[
                                                                                         html.Div(children=[
-                                                                                                            html.Iframe(id='boxplot',src = 'https://plot.ly/~Eekhoorn234/10.embed',style={'border': 'none', 'align': 'center', 'width': '100%', 'height':deel_heightScreen}),
-                                                                                                            html.Iframe(id='circlediagram',src = 'https://plot.ly/~Eekhoorn234/10.embed',style={'border': 'none', 'align': 'center', 'width': '100%', 'height':deel_heightScreen}),
-                                                                                                            html.Iframe(id='graph',src = 'https://plot.ly/~Eekhoorn234/10.embed',style={'border': 'none', 'align': 'center', 'width': '100%', 'height':deel_heightScreen})],style={'position':'relative', 'height':heightScreen},className='four columns'),
+                                                                                                            html.Div(children=[
+                                                                                                            html.H2(children=format(dict_links_numbers.get('sites')),
+                                                                                                                    id='number',
+                                                                                                                    style={'textAlign': 'center',
+                                                                                                                                 'color': 'fc8600',
+                                                                                                                                 'fontSize' : 50,
+                                                                                                                                 'fontFamily': 'verdana',
+                                                                                                                                 'backgroundColor' : 'white',
+                                                                                                                                 'border': 'none',
+                                                                                                                                 'paddingTop': fifteen_heightScreen}),
+                                                                                                             html.H3(children=format(dict_links_text.get('sites')),
+                                                                                                                    id='text',
+                                                                                                                    style={'textAlign': 'center',
+                                                                                                                                 'color': 'fc8600',
+                                                                                                                                 'fontSize' : 20,
+                                                                                                                                 'fontFamily': 'verdana',
+                                                                                                                                 'backgroundColor' : 'white',
+                                                                                                                                 'border': 'none',
+                                                                                                                                 'paddingTop': 0})],
+                                                                                                        style={'border': 'none', 'align': 'center', 'width': '100%', 'height':twoeight_heightScreen}),
+                                                                                                           # html.Iframe(id='number',src = 'https://plot.ly/~Eekhoorn234/10.embed',style={'border': 'none', 'align': 'center', 'width': '100%', 'height':deel_heightScreen}),
+                                                                                                            html.Iframe(id='boxplot',src = 'https://plot.ly/~Eekhoorn234/10.embed',style={'border': 'none', 'align': 'center', 'width': '100%', 'height':threeeight_heightScreen}),
+                                                                                                            html.Iframe(id='graph',src = 'https://plot.ly/~Eekhoorn234/10.embed',style={'border': 'none', 'align': 'center', 'width': '100%', 'height':threeeight_heightScreen})],style={'position':'relative', 'height':heightScreen},className='four columns'),
                                                                                         
                                                                                         html.Div(children=[
                                                                                                            html.Iframe(id='map_folium',srcDoc = open('maps/all_sites.html', 'r').read(),style={'border': 'none', 'align': 'center', 'width': '100%', 'height':heightScreen})],
@@ -99,6 +145,20 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},
 def update_map(select_maps):
     new_src = format(dict_links_maps.get(select_maps))
     return open(new_src, 'r').read()
+
+@app.callback(
+    Output(component_id='number', component_property='children'),
+    [Input(component_id='select_maps', component_property='value')])
+def update_numbers(select_maps):
+    new_no = format(dict_links_numbers.get(select_maps))
+    return new_no
+
+@app.callback(
+    Output(component_id='text', component_property='children'),
+    [Input(component_id='select_maps', component_property='value')])
+def update_text(select_maps):
+    new_text = format(dict_links_text.get(select_maps))
+    return new_text
 
 @app.callback(
     Output(component_id='boxplot', component_property='src'),
